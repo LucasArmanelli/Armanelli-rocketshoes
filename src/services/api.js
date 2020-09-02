ea187971502fd6b10b9d8dbeb048cb9ab47509e7
Lucas Armanelli <luc.armanelli@gmail.com>
2020-09-02 17:01:30 -0300

    att

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL:
+    'http://localhost:https://my-json-server.typicode.com/LucasArmanelli/Armanelli-rocketshoes',
 });
 
 export default api;
